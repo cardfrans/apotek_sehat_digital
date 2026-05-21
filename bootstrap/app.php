e252b3e1d19c98c19247ce27d9b103ec220288e3
cardfrans <cardinofransisco@gmail.com>
2026-05-21 22:47:41 +0700

    perbarui app.php bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,8 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: 'v1',
+
     )
     ->withMiddleware(function (Middleware $middleware) {
         // TAMBAHKAN BARIS INI UNTUK DAFTARKAN ALIAS MIDDLEWARE ADMIN
